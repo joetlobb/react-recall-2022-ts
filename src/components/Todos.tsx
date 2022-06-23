37c747725bdbd3983befae9e0c53feb470c18732
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-23 14:12:35 +0700

    adding styles

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,10 @@
 import TodoItem from "./TodoItem";
 import todo from "../models/todo";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem key={item.id} text={item.text} />
       ))}
